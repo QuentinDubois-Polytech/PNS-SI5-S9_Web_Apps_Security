d91a711ed3b287931d095c72bda9f66692fa5a42
Quentin Dubois <quentin.dubois@etu.univ-cotedazur.fr>
2024-01-13 18:44:54 +0100

    mise à jour du code tp5

--- src/tp5/mycode/attackerGadget.js
+++ src/tp5/mycode/attackerGadget.js
@@ -1,19 +1,8 @@
 (() => {
-    const protocol = "http";
+    const protocol = window.location.protocol;
+    const port = window.location.port;
     const domain = "host.com";
-
-    const protoToUse = {
-        "http": {
-            "protocol": "http:",
-            "port": "8080",
-        },
-        "https": {
-            "protocol": "https:",
-            "port": "4443",
-        }
-    }[protocol];
-
-    const url = `${protoToUse.protocol}//${domain}:${protoToUse.port}/tp5/ressources/simple.php`;
+    const url = `${protocol}//${domain}:${port}/tp5/ressources/simple.php`;
     window.location.replace(url)
 })();
 
