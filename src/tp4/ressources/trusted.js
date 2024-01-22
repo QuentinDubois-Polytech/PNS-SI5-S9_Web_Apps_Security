3bc9554f8e070b244f2981670ee09f4b537c4667
Quentin Dubois <quentin.dubois@etu.univ-cotedazur.fr>
2024-01-22 20:36:39 +0100

    fix missing xmlhttp var in trusted.js tp4

--- src/tp4/ressources/trusted.js
+++ src/tp4/ressources/trusted.js
@@ -1,4 +1,6 @@
 var url =  "http://host.com:8080/";
+var xmlhttp = new XMLHttpRequest();
+
 function callback(){console.log('response received')}; 
 var call = function () {
            xmlhttp.open('GET',url, true);
